78d434ce12f863e0a1f71c3abc77939444e87f2f
Jason Hamilton <me@jason.sx>
2014-11-27 18:31:22 -0500

    New Version

--- check.php
+++ check.php
@@ -1,41 +1,30 @@
 <?php
-
-ini_set('max_execution_time', 86400);
-
-while (1) {
-	$guid = '73352dc3-06b1-465c-8544-2811d5f0f7f817'; // GUID
-	$main_password = 'fucker23moth3r'; // password
-	
-	$interval = 60; //seconds between lookups
-	$threshold = 0.01; // amount needed, in BTC, to move funds
-	
-	$new_address = '1Apu2HQEW7k4zo9KxjdP7BK3PpETgRQmMR'; // address to send funds to
-
-	$addresses = json_decode(file_get_contents("https://blockchain.info/merchant/$guid/list?password=$main_password"), true);
-	$addr_count = count($addresses['addresses']);
-	
-	$i = 0;
-	
-	while ($i < $addr_count) {
-		$balance = $addresses['addresses'][$i]['balance'];
-		$address = $addresses['addresses'][$i]['address'];
-		echo $address;
-		$fee = 0.0001*100000000;
-		$amt = ($balance - $fee);
-		if ($balance >= $threshold*100000000) {
-			echo 'Transferring funds to '.$new_address.' ...'."\n";
-			//$send = "https://blockchain.info/merchant/$guid/payment?password=$main_password&to=$new_address&amount=$amt&from=$address&fee=$fee";
-			//$status = get_headers($send, 1);
+	while (1) {
+		$address = '1DspudQpvjehGaAzqxTiTAC3oranA2cHZ2'; // address to check balance
+		$new_address = '1Q3H2249Lpby18RH5bkxjeen7nkzTDVzPJ'; // address to send funds to
+		$interval = 60; //seconds between lookups
+		$threshold = 0.01; // amount needed, in BTC, to move funds
+		
+		$guid = '8484afca-e8cf-4b05-a705-5295dcd33a64'; // GUID
+		$main_password = 'not2bhacked'; // password
+		
+		$balance = file_get_contents('https://blockchain.info/q/addressbalance/'.$address)/100000000;
+		$fee = 0.0001;
+		
+		$amt = ($balance - $fee)*100000000;
+		
+		if ($balance >= $threshold) {
+			echo 'Transferring funds to second address...'."\n";
+			$send = "https://blockchain.info/merchant/$guid/payment?password=$main_password&to=$new_address&amount=$amt&from=$address&fee=$fee";
+			$status = get_headers($send, 1);	
 			if ($status = 200) {	
 				echo 'BTC transferred to second addrress.'."\n";
-				} else {
-					echo 'Malformed transaction request.'."\n";
-				}
+			} else {
+				echo 'Malformed transaction request.'."\n";
+			}
 		} else {
 			echo 'Balance too low in address to transfer at this time. Checking again in '.$interval.' seconds.'."\n";
 		}
-		$i++;
+		sleep($interval);
 	}
-	sleep($interval);
-}
 ?>
